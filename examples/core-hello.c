29db72bfcb25dcb60017ef3e54423b22cbe1b91d
tuorqai <tuorqai@gmail.com>
2022-07-28 17:18:37 +0900

    New core/hello example, old examples are also removed (including their assets).

---
+++ examples/core-hello.c
@@ -0,0 +1,57 @@
+
+//------------------------------------------------------------------------------
+
+#include <tq/tq.h>
+
+//------------------------------------------------------------------------------
+// [core/hello]
+// This example shows some basic usage of tq library.
+//------------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    // Set initial display size.
+    // It's safe to call this function before tq library is initialized.
+    tq_set_display_size((tq_vec2i) {512, 512});
+
+    // Set title of the window (has no effect on Android).
+    // It's safe to call this function before tq library is initialized.
+    tq_set_title("[tq library] core/hello");
+
+    // Initialize tq library.
+    // If it fails to initialize, abort() will be called and
+    // your application will be terminated.
+    tq_initialize();
+
+    // Use standard atexit() function to call tq_terminate()
+    // when your program ends.
+    atexit(tq_terminate);
+
+    // Set background color (used by tq_clear()).
+    tq_set_clear_color((tq_color) {0, 0, 30, 255});
+
+    // Set outline color (used by some graphics primitives,
+    // such as rectangles and circles).
+    tq_set_outline_color((tq_color) {180, 180, 0, 255});
+
+    // tq_process() will return `false` in two cases only:
+    // either when the user wants to exit or when some serious
+    // error happens.
+    // Otherwise, it will return `true`.
+    // This function should be called every frame, since it does
+    // a lot of things under the hood.
+    while (tq_process()) {
+        // Clear the screen using background color we set earlier.
+        tq_clear();
+
+        // Draw yellowish circle.
+        tq_outline_circle((tq_vec2f) {256, 256}, 100);
+    }
+
+    // As a result, you should see a square window with a yellow circle
+    // on the center.
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------
